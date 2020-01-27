a97e561816d7013d73c6d0d40291c0f19a1b1841
Eduardo Benitez <A01039203@itesm.mx>
2020-01-27 16:00:42 -0600

    Installed vue-js-modal (#16)
    * Installed vue-js-modal
    
    * Included VModal plugin to main.ts file

--- pos-dev/src/main.ts
+++ pos-dev/src/main.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VModal from 'vue-js-modal'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(VModal)
 Vue.config.productionTip = false
 
 new Vue({
