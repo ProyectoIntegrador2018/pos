1df7d828673959137fcfbb6c6785c39f095bb55b
denisse96 <denissedam@gmail.com>
2020-05-24 21:25:04 -0500

    Modified Controllers GetBalance

--- POS-netcore/POS/Controllers/GetBalanceController.cs
+++ POS-netcore/POS/Controllers/GetBalanceController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using POS.Models;
+
 
 namespace POS.Controllers
 {
@@ -17,30 +19,18 @@ namespace POS.Controllers
         [HttpGet]
         public string Get()
         {
-            var jsonString = @"{
-                ""ResponseCode"":""000"",
-                ""ResponseDescription"":""TxnSuccessful"",
-                ""ServiceList"":{
-                    ""ServiceID"":[""10"",""81"",""30"",""66"",""28"",""17""],
-                    ""ServiceName"":[""TopUp"", ""GetBalance"",""BillPay"",""PinSell"",""GetEnterpriseList"",""GetProductsDetails""],
-                    ""ServiceCode"":[""S2S"",""S2NR"",""S2EC"",""S2A"",""S2O"",""S2M""],
-                    ""ServiceType"":[""P"",""P"",""P"",""P"",""P"",""P""],
-                    ""ServiceDenominationID"":[],
-                    ""MinAmount"":[""250.010000"",""1.000000"",""1.000000"",""1.000000"",""1000.010000"",""1.000000""],
-                    ""MaxAmount"":[""500.000000"",""10000.000000"",""5000.000000"",""200.000000"",""5000.000000"",""1000.000000""],
-                    ""IsActive"":[""1"",""1"",""1"",""1"",""1"",""1""],""InternalModuleID"":[""6"",""6"",""4"",""7"",""7"",""8""],
-                    ""ModuleName"":[""Transfer Money"",""Transfer Money"",""Business Payment"",""Cash Out"",""CashOut"",""MerchantPayment""],
-                    ""ServiceImage"":[""e91e"",""e91e"",""e90a"",""e90c"",""e90c"",""e913""],
-                    ""ServiceIconWeb"":[""ms-icon-transfer-money"",""ms-icon-transfer-money"",""ms-icon-business-payment"",""ms-icon-cash-out"",""ms-icon-cash-out"",""ms-icon-merchant-payment""],
-                    ""ServiceIconMob"":[""e91e"",""e91e"",""e90a"",""e90c"",""e90c"",""e913""],
-                }
-            }";
+            pagoFonApi llamada1 = new pagoFonApi();
+            var result = llamada1.GetBalance();
+            Console.WriteLine(result + "Result");
+
+
 
             // This is intended to simulate the deserialization and serialization of
             // the actual response from PagoFon.
-            var response = JsonConvert.DeserializeObject<PagoFonResponse>(jsonString);
+            var response = JsonConvert.DeserializeObject<GetBalanceResponse>(result);
 
             return JsonConvert.SerializeObject(response);
+
         }
     }
 
