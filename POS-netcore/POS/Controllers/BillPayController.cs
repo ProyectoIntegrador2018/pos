f2c76c366318dc2eb0776bdbd6f342cf54dc8ec6
Andrés Villanueva <andresrvg10@gmail.com>
2020-05-24 21:20:01 -0500

    Bill pay (#40)
    * Bill Pay Controller and Models for endpoints
    
    * BillPay Already Working

---
+++ POS-netcore/POS/Controllers/BillPayController.cs
@@ -0,0 +1,41 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using POS.Models;
+using System.IO;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Web;
+using System.Net;
+
+namespace POS.Controllers
+{
+    [Route("api/BillPay")]
+    [ApiController]
+    public class BillPayController : ControllerBase 
+    {
+        /// <summary>
+        /// Gets the dummy data for ProductsController GET request
+        /// </summary>
+        /// <returns>The list of dummy products</returns>
+        [HttpPost]
+        public string PostBillPayTransaction(BillPayRequest request){
+
+            pagoFonApi billpay = new pagoFonApi();
+
+            var result = billpay.BillPay(request.ProductCode,request.BillPayData,request.Amount,request.MPin,request.Email,request.ANI);
+            
+            var response = JsonConvert.DeserializeObject<BillPayResponse>(result);
+            return JsonConvert.SerializeObject(response);
+
+        }
+    }
+
+    
+}
---
+++ POS-netcore/POS/Models/BillPayRequest.cs
@@ -0,0 +1,36 @@
+using System;
+namespace POS.Models
+{
+    public class BillPayRequest
+    {
+        public string ProductCode { get; set; }
+        public string BillPayData { get; set; }
+        public string Amount { get; set; }
+        public string MPin { get; set; }
+        public string Email { get; set; }
+        public string ANI { get; set; }
+
+        
+        /// <summary>
+        /// TransactionRequest based on documentation provided by PagoFon, on method Topup
+        /// This only accounts for the information provided by the costumer, the other
+        /// parameteres should be environment variables from the POS
+        /// </summary>
+        public BillPayRequest(string ProductCode, string BillPayData,string Amount, string MPin, string Email, string ANI)
+        {
+            this.ProductCode = ProductCode;
+            this.BillPayData = BillPayData;
+            this.Amount = Amount;
+            this.MPin = MPin;
+            this.Email = Email;
+            this.ANI = ANI;
+
+
+        }
+
+        /// <summary>
+        /// Parameterless class definition for JSON deserialization
+        /// </summary>
+        public BillPayRequest() { }
+    }
+}
